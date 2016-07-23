188cc1696ebc9fe0a5e4e9d271656759fa999c18
Thomas Keel <sndbytgkv1@verizon.net>
2016-07-23 00:27:07 -0400

    added console.table for display, and update product tbl

--- bamOrig02.js
+++ bamOrig02.js
@@ -1,6 +1,7 @@
 //INITIALIZES THE NPM PACKAGES USED//
 var mysql    = require('mysql');
 var inquirer = require('inquirer');
+require('console.table');
 
 var clrScreen = true;
 
@@ -30,15 +31,17 @@ var makeTable = function() {
     //SELECTS ALL OF THE DATA FROM THE MYSQL PRODUCTS TABLE - SELECT COMMAND!
     connection.query('SELECT * FROM products', function(err, res) {
         if (err) throw err;
+        console.table(res);
         //PRINTS THE TABLE TO THE CONSOLE WITH MINIMAL STYLING//
-        var tab = "\t";
-        console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
-        console.log("--------------------------------------------------------");
+        //var tab = "\t";
+        //console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
+        //console.log("--------------------------------------------------------");
         //FOR LOOP GOES THROUGH THE MYSQL TABLE AND PRINTS EACH INDIVIDUAL ROW ON A NEW LINE//
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
-        }
-        console.log("--------------------------------------------------------");
+        //for (var i = 0; i < res.length; i++) {
+        //    console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
+        //}
+        //console.log("--------------------------------------------------------");
+        console.log("\n");
         //RUNS THE CUSTOMER'S PROMPTS AFTER CREATING THE TABLE. SENDS res SO THE promptCustomer FUNCTION IS ABLE TO SEARCH THROUGH THE DATA//
         promptCustomer(res);
     });
@@ -50,32 +53,58 @@ var promptCustomer = function(res) {
         inquirer.prompt([{
             type: 'input',
             name: 'choice',
-            message: 'What would you like to purchase?'},
+            message: 'What item would you like to order? ==> '},
             {
             type: 'input',       
             name: 'qty',
-            message: 'How many would you like to purchase?'    
+            message: '          and your order quantity? ==> '    
         }]).then(function(val) {
 
                 //SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
                 var correct = false;
 
-                console.log("res.length1=" + res.length);
-                console.log("val.choice="  + val.choice);
-                console.log("val.qty="     + val.qty);
+                //console.log("res.length1=" + res.length);
+                //console.log("val.choice="  + val.choice);
+                //console.log("val.qty="     + val.qty);
 
                 //LOOPS THROUGH THE MYSQL TABLE TO CHECK THAT THE PRODUCT THEY WANTED EXISTS//
-                for (var i = 0; i < res.length; i++) {                    	
+                for (var i = 0; i < res.length; i++) {     
+                    correct = false;
+
 	                //1. TODO: IF THE PRODUCT EXISTS, SET correct = true and ASK THE USER TO SEE HOW MANY OF THE PRODUCT THEY WOULD LIKE TO BUY//
                     //console.log(typeof res[i].ItemID);
                     if (res[i].ItemID == parseInt(val.choice)) {
-                        correct == true;
-                        console.log("Item Found : i=" + i +  " "  + res[i].ProductName);
+                        correct = true;
+                        //console.log("Item Id=" + res[i].ItemID + " - " + res[i].ProductName + " found.");
                     } 
 	               	//2. TODO: CHECK TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE// 
-                    console.log( typeof res[i].StockQuanaity);
-                    if (correct == true && res[i].StockQuanaity >= parseInt(val.qty)) {
-                         console.log("Item StockQuanaity is sufficient : i=" + i + " " + res[i].ProductName );
+                    //console.log( typeof res[i].StockQuantity);
+                    //console.log("correct=" + correct);
+                    //console.log("val.qty=" + val.qty);
+                    if (correct == true && res[i].StockQuantity >= parseInt(val.qty)) {
+                        console.log("\n");
+                        console.log("    Item #" + res[i].ItemID + " - " + res[i].ProductName 
+                            + " - quantity " + val.qty + " has been ordered.");
+
+
+                        var newQty = res[i].StockQuantity - val.qty;
+                        connection.query("UPDATE products SET StockQuantity='"+newQty+"' WHERE ItemId='"+val.choice+"'", function(err, res) {
+                            if (err) throw err;
+                        });
+                        connection.query('SELECT * FROM `products` WHERE `ItemID` = ?', [val.choice], function (err, res) {
+                            if (err) throw err;
+                            console.table(res);
+                        });
+
+                        //"UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+                        
+                    }
+
+                    if (correct == true && res[i].StockQuantity <= 0) {
+                        console.log("\n");
+                        console.log("    Item #" + res[i].ItemID + " - " + res[i].ProductName 
+                            + " - quantity " + val.qty + " is temporarily out of stock.");
+                        // console.log("  Item StockQuanaity is sufficient : i=" + i + " " + res[i].ProductName );
                     }
 
 	                //3. TODO: UPDATE THE MYSQL TO REDUCE THE StockQuanaity by the THE AMOUNT REQUESTED  - UPDATE COMMAND!
@@ -86,9 +115,10 @@ var promptCustomer = function(res) {
                 }
 
                 //IF THE PRODUCT REQUESTED DOES NOT EXIST, RESTARTS PROMPT//
-                if (i == res.length && correct == false) {
+               //if (i == res.length && correct == false) {
+                    console.log("\n");
                     promptCustomer(res);
-                }
+                //}
             });
 }
 
